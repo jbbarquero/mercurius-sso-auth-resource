ec0252551a1d31088eccf66d472aaa5866b8d374
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-10-22 06:51:56 +0200

    OAuth2 Resource empty (just created the project)

---
+++ src/main/java/com/malsolo/mercurius/oauth2/resource/MercuriusSsoResourceApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.mercurius.oauth2.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuriusSsoResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuriusSsoResourceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/malsolo/mercurius/oauth2/resource/MercuriusSsoResourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.malsolo.mercurius.oauth2.resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MercuriusSsoResourceApplication.class)
+public class MercuriusSsoResourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
